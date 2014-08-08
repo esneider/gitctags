19354506997d31f153f61373aa32692308e628d2
esneider <dariosn@gmail.com>
2014-08-08 11:44:07 -0300

    Add the language field to the tags output

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ cat > ctags << END
 set -e
 PATH="/usr/local/bin:\$PATH"
 trap "rm -f .git/tags.\$$" EXIT
-ctags --tag-relative -Rf.git/tags.\$$ --exclude=.git
+ctags --tag-relative -Rf.git/tags.\$$ --exclude=.git --fields=+l
 mv .git/tags.\$$ .git/tags
 END
 
